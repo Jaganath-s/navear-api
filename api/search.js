73927f73ec18824308dd2791002ca3bb8304d5f4
Jaganath S <143887190+Jaganath-s@users.noreply.github.com>
2026-04-19 17:22:18 +0530

    Implement OAuth token retrieval for API search
    Refactor search function to include OAuth token retrieval and update API call with authorization header.

--- api/search.js
+++ api/search.js
@@ -4,17 +4,40 @@ const query = req.query.q
 const lat = req.query.lat
 const lon = req.query.lon
 
-const MAPPLS_KEY = "b7d84b6cfc33e01bbf1d544e6dedc77a"
-
-const url =
-`https://atlas.mappls.com/api/places/nearby/json?keywords=${query}&refLocation=${lat},${lon}&radius=2000&access_token=${MAPPLS_KEY}`
+const CLIENT_ID = "96dHZVzsAutmZhjoahEwrn5buFh4UYyBousoCGUQylaYN3vImZ4_-HAPb9eZ4_iKrot-c7eoBQl9cXN2yaJ0sA=="
+const CLIENT_SECRET = "lrFxI-iSEg_bdmFqn7LhmTyqHeQ3dspgw5_TNAZqv1OUhIRqyxeGdLPMv6V5NNLkweThtK2aSCeO2ZJ6rkGheSbVd0fKhruv"
 
 try {
 
-const r = await fetch(url)
-const data = await r.json()
+// STEP 1 — Get OAuth token
+const tokenRes = await fetch(
+"https://outpost.mappls.com/api/security/oauth/token",
+{
+method: "POST",
+headers: {
+"Content-Type": "application/x-www-form-urlencoded"
+},
+body:
+`grant_type=client_credentials&client_id=${CLIENT_ID}&client_secret=${CLIENT_SECRET}`
+}
+)
 
-if (!data.suggestedLocations || data.suggestedLocations.length === 0) {
+const tokenData = await tokenRes.json()
+const token = tokenData.access_token
+
+// STEP 2 — Search nearby place
+const searchURL =
+`https://atlas.mappls.com/api/places/nearby/json?keywords=${query}&refLocation=${lat},${lon}&radius=2000`
+
+const searchRes = await fetch(searchURL,{
+headers:{
+Authorization: `Bearer ${token}`
+}
+})
+
+const data = await searchRes.json()
+
+if(!data.suggestedLocations || data.suggestedLocations.length === 0){
 res.json({error:"No place found"})
 return
 }
@@ -28,9 +51,10 @@ lon: place.longitude
 })
 
 }
-catch(e)
-{
-res.json({error:"API failed"})
+catch(e){
+
+res.json({error:"Search failed"})
+
 }
 
 }
